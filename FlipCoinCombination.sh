386a77d95376e12d5d1e9718075596d92689184d
Pratik Chaudhari <pratikc1020@gmail.com>
2021-05-31 17:39:48 +0530

    Added feature to display heads or tails

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
-echo "Welcome to Flip Coin Combination using shell script"
+echo "Welcome to flip coin simulation : "
+sleep 1
+
+#Generating a random number.
+random=$(( RANDOM%2 ));
+
+if [ $random -eq 0 ];
+then
+	echo "Heads wins!";
+else
+       	echo "Tails Wins!";
+fi
