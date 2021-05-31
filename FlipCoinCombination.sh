d6fd068c8da1d1162517be9f4ce7dd8f67cd2554
Pratik Chaudhari <pratikc1020@gmail.com>
2021-05-31 18:24:16 +0530

    Stored results in dictionary, Added feature of Percentage win

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,14 +1,53 @@
 #!/bin/bash -x
 
-echo "Welcome to flip coin combination : "
+#Welcome message:
+echo "Welcome to flip coin combination"
 sleep 1
 
-#Generating a random number.
-random=$(( RANDOM%2 ));
+#Getting limit value from the user:
+read -p "How many rounds do you wish to play? " limit
 
-if [ $random -eq 0 ];
+Heads=0;
+Tails=0;
+
+#Declaring a dictionary:
+declare -A Results
+
+for (( i=1; i<=$limit; i++ ));
+do
+	random=$(( RANDOM%2 ));
+
+	if [ $random -eq 0 ];
+	then
+		Heads=$(( $Heads + 1 ));
+		Results["Heads"]=$Heads		#Storing result in dictionary
+	else
+		Tails=$(( $Tails + 1 ));
+		Results["Tails"]=$Tails		#Storing result in dictionary
+	fi
+done
+
+
+#Displaying result:
+echo "Heads came ${Results[Heads]} times"
+echo "Tails came ${Results[Tails]} times"
+echo "=================================="
+
+
+#Determining win percentage:
+if [ $Heads -gt $Tails ];
 then
-	echo "Heads wins!";
-else
-       	echo "Tails Wins!";
+	Percentage=$(( ($Heads*100)/$limit ));
+	echo "Heads Won by $Percentage%."
+
+elif [ $Tails -gt $Heads ]
+then
+	Percentage=$(( ($Tails*100)/$limit ));
+	echo "Tails won by $Percentage%."
+
+elif [ $Tails -eq $Heads ];
+then
+	echo "It's a Tie!"
 fi
+
+
