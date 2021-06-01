b825f132a0525512ea6d426bc2708c6c50fcb463
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-01 19:08:41 +0530

    Added feature Doublet

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,53 +1,51 @@
-#!/bin/bash -x
+#! /bin/bash -x
+
+Count=1; sum=0; HH=0; HT=0; TH=0; TT=0;
 
 #Welcome message:
-echo "Welcome to flip coin combination"
+echo "Welcome to Doublet coin flip "
 sleep 1
 
 #Getting limit value from the user:
-read -p "How many rounds do you wish to play? " limit
+read -p "How many times do you wish to toss two coins : " num
 
-Heads=0;
-Tails=0;
-
-#Declaring a dictionary:
-declare -A Results
-
-for (( i=1; i<=$limit; i++ ));
+while [ $Count -le $num ]
 do
-	random=$((RANDOM%2));
+        #Generating two random values, each simulating a coin flip:
+        rand1=$((RANDOM%2))
+        rand2=$((RANDOM%2))
 
-	if [ $random -eq 0 ];
-	then
-		Heads=$(($Heads+1));
-		Results["Heads"]=$Heads		#Storing result in dictionary
-	else
-		Tails=$(($Tails+1));
-		Results["Tails"]=$Tails		#Storing result in dictionary
-	fi
+                if [ $rand1 -eq 1 ] && [ $rand2 -eq 1 ]
+                then
+                        CoinFlip["sum"]='HH'
+                        HH=$((HH+1))
+
+                elif [ $rand1 -eq 0 ] && [ $rand2 -eq 1 ]
+                then
+                        CoinFlip["sum"]='TH'
+                        ((TH++))
+                elif [ $rand1 -eq 0 ] && [ $rand2 -eq 0 ]
+                then
+                        CoinFlip["sum"]='TT'
+                        ((TT++))
+                elif [ $rand1 -eq 1 ] && [ $rand2 -eq 0 ]
+                then
+                        CoinFlip["sum"]='HT'
+                        ((HT++))
+                fi
+((sum++))
+((Count++))
+
 done
 
+echo ${CoinFlip[*]}
 
-#Displaying result:
-echo "Heads came ${Results[Heads]} times"
-echo "Tails came ${Results[Tails]} times"
-echo "=================================="
-
-
-#Determining win percentage:
-if [ $Heads -gt $Tails ];
-then
-	Percentage=$(( ($Heads*100)/$limit ));
-	echo "Heads Won by $Percentage%."
-
-elif [ $Tails -gt $Heads ]
-then
-	Percentage=$(( ($Tails*100)/$limit ));
-	echo "Tails won by $Percentage%."
-
-elif [ $Tails -eq $Heads ];
-then
-	echo "It's a Tie!"
-fi
-
-
+#Determining Win percentage:
+HHPercent=$(echo $HH $num | awk '{print $1/$2*100}')
+echo "Percent of doublet HH :" $HHPercent
+TTPercent=$(echo $TT $num | awk '{print $1/$2*100}')
+echo "Percent of doublet TT :" $TTPercent
+THPercent=$(echo $TH $num | awk '{print $1/$2*100}')
+echo "Percent of doublet TH :" $THPercent
+HTPercent=$(echo $HT $num | awk '{print $1/$2*100}')
+echo "Percent of doublet HT :" $HTPercent
